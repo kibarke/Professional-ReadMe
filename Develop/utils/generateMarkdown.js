56f7d1e6f9c1c779db70384758ab43b8c56ad6ec
kibarke <kibarkework@gmail.com>
2024-02-24 16:57:44 -0500

    filled out the index.js and the generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,72 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function to render license badge
+function renderLicenseBadge(license) {
+  if (license) {
+    return `[![License: ${license}](https://img.shields.io/badge/License-${license}-blue.svg)](https://opensource.org/licenses/${license})`;
+  } else {
+    return '';
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function to render license link
+function renderLicenseLink(license) {
+  if (license) {
+    return `[License](#license)`;
+  } else {
+    return '';
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function to render license section of README
+function renderLicenseSection(license) {
+  if (license) {
+    return `## License
 
-// TODO: Create a function to generate markdown for README
+This project is licensed under the [${license} License](https://opensource.org/licenses/${license}).`;
+  } else {
+    return '';
+  }
+}
+
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+${renderLicenseLink(data.license)}
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+For questions about the project, you can reach me at [${data.email}](mailto:${data.email}).
+
+${renderLicenseSection(data.license)}
 `;
 }
 
